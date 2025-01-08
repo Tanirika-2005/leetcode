b141e99e0e2fe263af2dfb532d3cf409a135aed3
Tanirika-2005 <88628736+Tanirika-2005@users.noreply.github.com>
2025-01-09 00:47:14 +0530

    Time: 3 ms (87.23%), Space: 71.6 MB (38.41%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        // Helper function to count subarrays with at most `k` odd numbers
         return countSubarraysWithAtMostK(nums, k) - countSubarraysWithAtMostK(nums, k - 1);
     }
     
     int countSubarraysWithAtMostK(vector<int>& nums, int k) {
-        int count = 0;  // To store the number of valid subarrays
-        int oddCount = 0;  // To store the number of odd numbers in the current window
-        int l = 0;  // Left pointer of the sliding window
-        
-        // Iterate through the array with the right pointer `r`
+        int count = 0;  
+        int oddCount = 0;  
+        int l = 0;  
         for (int r = 0; r < nums.size(); r++) {
-            // If the current element is odd, increase the odd count
             if (nums[r] % 2 != 0) {
                 oddCount++;
             }
-            
-            // If the number of odd numbers exceeds `k`, move the left pointer `l`
-            // to shrink the window until the number of odd numbers is less than or equal to `k`
             while (oddCount > k) {
                 if (nums[l] % 2 != 0) {
                     oddCount--;
@@ -26,7 +19,6 @@ public:
                 l++;
             }
             
-            // Add the number of subarrays that end at index `r` and have at most `k` odd numbers
             count += (r - l + 1);
         }
         
